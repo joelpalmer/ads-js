4f88fea687d015f66365799c1d97b36d660bf000
Joel Palmer <joelpalmer@gmail.com>
2019-03-12 14:15:34 -0500

    add countUniqueValues // multiple pointers

---
+++ problem_solving/countUniqueValues.js
@@ -0,0 +1,21 @@
+// multiple pointers
+
+function countUniqueValues(arr) {
+	let i = 0;
+	for (let j = 1; j < arr.length; j++) {
+		if (arr[i] !== arr[j]) {
+			i++;
+			arr[i] = arr[j];
+		}
+		
+    }
+    return i + 1;
+}
+
+const arr1 = [1, 1, 2, 3, 3, 4, 5, 6, 6, 7].sort();
+const arr2 = [1, 1, 2, 3, 3, 4, 5, 6, 6, 7, 8].sort();
+const arr3 = [1, 1, 2, 3, 3, 4, 5, 6, 6, 7, 9, 9].sort();
+
+countUniqueValues(arr1); //?
+countUniqueValues(arr2); //?
+countUniqueValues(arr3); //?
