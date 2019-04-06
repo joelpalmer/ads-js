3acd2b9512ee407bade2044600b67c31592b851c
Joel Palmer <joelpalmer@gmail.com>
2019-04-06 15:15:18 -0500

    finish insertionSort

--- sorting/perfTest.js
+++ sorting/perfTest.js
@@ -1,6 +1,9 @@
 const bubbleSort = require('./bubble');
 const selectionSort = require('./selection');
+const insertionSort = require('./insertion');
 const nums = [2, 4, 22, 23, 1, 16, 24, 32, 3, 7, 19, 4];
-const nums2 = [2, 4, 22, 23, 1, 16, 24, 32, 3, 7, 19, 4];
+const nums2 = [...nums];
+const nums3 = [...nums];
 bubbleSort(nums); //?.$
 selectionSort(nums2); //?.$
+insertionSort(nums3); //?.$
