c31d0150b7ad5fcacd5e81769f07e3d461650a01
Joel Palmer <joelpalmer@gmail.com>
2019-04-06 14:03:05 -0500

    add bubble sort

---
+++ sorting/bubble.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) { // not optimized
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // swap
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+
+        }
+    }
+    return arr;
+}
+
+module.exports = bubbleSort;
---
+++ sorting/perfTest.js
@@ -0,0 +1,3 @@
+const bubbleSort = require('./bubble');
+const nums = [4, 23, 1, 16, 24, 32, 3, 7, 19, 4];
+bubbleSort(nums); //?.$
