aae2047e0c2aa80c26a6045d259a992fe7e4ed38
Joel Palmer <joelpalmer@gmail.com>
2019-04-06 14:12:18 -0500

    optimize bubble sort

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -1,6 +1,6 @@
-function bubbleSort(arr) { // not optimized
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
+function bubbleSort(arr) {
+    for (let i = arr.length; i > 0; i--) {
+        for (let j = 0; j < i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 // swap
                 let temp = arr[j];
--- sorting/perfTest.js
+++ sorting/perfTest.js
@@ -1,3 +1,3 @@
 const bubbleSort = require('./bubble');
-const nums = [4, 23, 1, 16, 24, 32, 3, 7, 19, 4];
+const nums = [4, 22, 1, 16, 24, 32, 3, 7, 19, 4];
 bubbleSort(nums); //?.$
